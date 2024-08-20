269e5985128e9c62a92aa1ea727c096a86cde0db
Sj3777 <sudhakarjha819@gmail.com>
2024-08-20 19:09:23 +0530

    added-acces-token

--- src/axios.ts
+++ src/axios.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import Cookies from 'js-cookie';
 
 const axiosInstance = axios.create({
   baseURL: "http://localhost:3000/api/v1",
@@ -10,6 +11,10 @@ const axiosInstance = axios.create({
 
 axiosInstance.interceptors.request.use(
   (config) => {
+    const token = Cookies.get("accessToken");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
     return config;
   },
   (error) => {
